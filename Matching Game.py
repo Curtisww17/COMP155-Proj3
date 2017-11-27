c599e0448e9e005fba6c0c235a9651836b6ecbb7
Curtisww17 <wcvikingdude@gmail.com>
2017-11-27 17:02:16 -0500

    new test to see if this works

--- Matching Game.py
+++ Matching Game.py
@@ -7,7 +7,8 @@ def Init():
     window = pygame.display.set_mode((800,500))
     pygame.display.set_caption('Matching Game')
     font1 = pygame.font.SysFont('broadway',32)
-    window.fill((0,0,0))
+    window.fill((255,255,255))
+
 
 
 
@@ -17,6 +18,7 @@ def Init():
 Init()
 
 while True:
+    window.blit(pygame.load('square.png'),200,200)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
