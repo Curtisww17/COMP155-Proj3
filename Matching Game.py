91beb9debf04f9b9d17de4a1fb226e258c35debf
Curtisww17 <wcvikingdude@gmail.com>
2017-11-27 16:46:07 -0500

    This is an initial update

--- Matching Game.py
+++ Matching Game.py
@@ -2,9 +2,18 @@ import pygame, sys, random
 from pygame.locals import *
 
 
+def Init():
+    pygame.init()
+    window = pygame.display.set_mode((800,500))
+    pygame.display.set_caption('Matching Game')
+    font1 pygame.font.SysFont('broadway',32)
 
 
 
+
+
+
+Init()
 
 while True:
     for event in pygame.event.get():
