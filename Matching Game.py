fae2448d02feb066b2bbab8807aaf6d86d52635a
Curtisww17 <wcvikingdude@gmail.com>
2017-11-27 23:54:33 -0500

    big update

--- Matching Game.py
+++ Matching Game.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 
 def Init():
-    global window, circle, cross, triangle, square, pentagon, hexagon, rhombus, purple, green, pink, blue, red, yellow, title, titleRect, font1, font2, text
+    global window, circle, cross, triangle, square, pentagon, hexagon, rhombus, purple, green, pink, blue, red, yellow, difficulty, title, titleRect, font1, font2, text
     pygame.init()
     window = pygame.display.set_mode((800,600))
     pygame.display.set_caption('Matching Game')
@@ -35,6 +35,8 @@ def Init():
     hexagon = pygame.transform.scale(pygame.image.load('hexagon.png'),(100,100))
     rhombus = pygame.transform.scale(pygame.image.load('rhombus.png'),(100,100))
 
+    difficulty = 0
+
 
     Start()
 
@@ -43,6 +45,9 @@ def Init():
 
     
 def Start():
+    global difficulty
+    mousex,mousey = 0,0
+    
     window.fill(blue)
     pygame.draw.rect(window, purple, (100,100, 600, 400))
     pygame.draw.rect(window, green, (125,175, 150, 300))
@@ -50,31 +55,100 @@ def Start():
     pygame.draw.rect(window, red, (525,175, 150, 300))
 
     diff = font2.render(text[1],True, (0,0,0))
-    diffRect = title.get_rect()
-    diffRect.center = (475,150)
+    diffRect = diff.get_rect()
+    diffRect.center = (400,125)
+    
+    easy = font2.render(text[2],True, (0,0,0))
+    easyRect = easy.get_rect()
+    easyRect.center = (200,300)
+    
+    med = font2.render(text[3],True, (0,0,0))
+    medRect = med.get_rect()
+    medRect.center = (400,300)
+    
+    hard = font2.render(text[4],True, (0,0,0))
+    hardRect = hard.get_rect()
+    hardRect.center = (600,300)
+    
 
     window.blit(title,titleRect)
     window.blit(diff,diffRect)
+    window.blit(easy,easyRect)
+    window.blit(med,medRect)
+    window.blit(hard,hardRect)
 
+    pygame.display.update()
+    
+    while difficulty == 0:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == MOUSEBUTTONUP:
+                mousex,mousey = event.pos
+        if mousey > 175 and mousey < 475:
+            if mousex > 125 and mousex < 275:
+                difficulty = 3
+            elif mousex > 325 and mousex < 475:
+                difficulty = 5
+            elif mousex > 525 and mousex < 675:
+                difficulty = 7
+    #print(difficulty)
     
     
-
-
 class Tile(object):
     def __init__(self, shape, color):
         self.shape = shape
         self.color = color
 
+def MakeGrid():
+    global grid
+    grid = []
+    minilist = []
+    colorlist = [red, yellow, green, blue, purple]
+    if difficulty == 3:
+        shapelist = [circle, cross, triangle]
+    if difficulty == 5:
+        shapelist = [circle, cross, triangle, square, pentagon]
+    if difficulty == 7:
+        shapelist = [circle, cross, triangle, square, pentagon, hexagon, rhombus]
+
+    for shape in shapelist:
+        minilist = []
+        for color in colorlist:
+            newtile = Tile(shape, color)
+            minilist.append(newtile)
+        grid.append(minilist)
+
+def DrawGrid():
+    window.fill(pink)
+    if difficulty == 3:
+        pass
+    if difficulty == 5:
+        pass
+    if difficulty == 7:
+        pass
+    for x in range(0,len(grid)):
+        for y in range(0,len(grid[x])):
+            pygame.draw.rect(window, grid[x][y].color, (x*100,y*100+100,100,100))
+            window.blit(grid[x][y].shape,(x*100,y*100+100))
+    pygame.display.update()
+            
+
 
 Init()
+MakeGrid()
+DrawGrid()
+
+
 
 while True:
-    window.blit(circle,(200,100))
-    window.blit(cross,(200,200))
-    window.blit(triangle,(200,300))
-    window.blit(square,(200,400))
-    window.blit(pentagon,(200,500))
-    window.blit(hexagon,(200,0))
+    #window.blit(circle,(200,100))
+    #window.blit(cross,(200,200))
+    #window.blit(triangle,(200,300))
+    #window.blit(square,(200,400))
+    #window.blit(pentagon,(200,500))
+    #window.blit(hexagon,(200,0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
